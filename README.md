7aab2e6736b03c6214bc7932eb45418e06455178
lizhanqiang@kanzhun.com <lizhanqiang@kanzhun.com>
2017-09-04 14:09:56 +0800

    lzq

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# adminLte-vue
+## Demo Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+### 修改
+#### admin-lte/dist/css/skins/_all-skins.min.css
+```
+.skin-blue-light .main-header .navbar {
+    background-color: #01C0C8
+}
+.skin-blue-light .main-header .logo {
+    background-color: #01B8C0;
+    color: #fff;
+    border-bottom: 0 solid transparent
+}
+```
+
+#### webpack.config.js 删掉
+```
+{
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: "pre",
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+```
---
+++ src/Slider.vue
@@ -0,0 +1,49 @@
+<template>
+  <aside id="slider" class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <va-slide-item
+          v-for="item in slideMenuItems"
+          :type="item.type"
+          :isHeader="item.isHeader"
+          :icon="item.icon"
+          :name="item.name"
+          :badge="item.badge"
+          :items="item.items"
+          :router="item.router"
+          :link="item.link"
+        >
+        </va-slide-item>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import VASlideItem from './components/VASlideItem'
+
+export default {
+  name: 'va-slider',
+  props: {
+    slideMenuItems: {
+      type: Array,
+      default: []
+    }
+  },
+  created () {
+
+  },
+  computed: {
+    ...mapGetters([
+      'currentUser'
+    ])
+  },
+  components: {
+    'va-slide-item': VASlideItem
+  }
+}
+</script>
