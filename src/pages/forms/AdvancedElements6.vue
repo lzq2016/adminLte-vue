c01a51ed7a3ffdcab7416190ea870110f06d2825
lzq2016 <357881371@qq.com>
2017-09-20 00:26:27 +0800

    lzq

--- src/pages/forms/AdvancedElements6.vue
+++ src/pages/forms/AdvancedElements6.vue
@@ -1,30 +1,73 @@
 <template>
 	
 	<section class="page-section">	
-			
+		<div class="header">
+			<div style="display: inline-block;float: left;margin-left: 20px;">
+				<img src="../../assets/grey.png" style="width: 11px;height: 11px;">
+				<span style="margin-right: 10px;">正常</span>
+				<img src="../../assets/green.png" style="width: 11px;height: 11px;">
+				<span style="margin-right: 10px;">已提交</span>
+				<img src="../../assets/pink.png" style="width: 11px;height: 11px;">
+				<span style="margin-right: 10px;">紧急超时</span>
+			</div>
+          <span style="font-size: 14px;color: #969696;">项目管理/</span>
+          <span style="font-size: 14px;color: #51D4D9;">项目完成额</span>
+        </div>	
 		<!-- row1 - 工作台 -->
-		<Row :gutter="16" style="margin-bottom: 16px;">
+		<Row :gutter="16" style="margin-bottom: 16px;margin-left: 10px;">
 			<Col span="12">
 				<div class="ivu-hse-table">
-					<h4>我的工作台</h4>
-					<Table stripe :columns="table1.th" :data="table1.td" :row-class-name="rowClassName"></Table>
+					<h4 style="font-size: 18px;">我的工作台</h4>
+					<!-- <Table stripe :columns="table1.th" :data="table1.td" :row-class-name="rowClassName"></Table> -->
+					<div class="tableWrap" v-bind:class="{ showHide: showHide1 }" style="margin-top: 10px;">
+                            <div class="tableRow backgroundGrey" style="font-weight: bold;height: 40px;">
+                                <span class="tableSpan" style="height: 40px;line-height: 40px;">项目名称</span>
+                                <span class="tableSpan" style="height: 40px;line-height: 40px;">立项时间</span>
+                                <span class="tableSpan" style="height: 40px;line-height: 40px;">任务类型</span>
+                            </div>
+                            <div v-for="(item, index) in data6">
+                                <div class="tableRow" :class="{ backgroundGrey: item.backgroundGrey,backgroundGreen:item.backgroundGreen,backgroundPink:item.backgroundPink }">
+                                    <span class="tableSpan">{{item.plantformname}}</span>
+                                    <span class="tableSpan">{{item.plantformtime}}</span>
+                                    <span class="tableSpan">{{item.plantformtype}}</span>
+                                </div>
+                            </div>
+                    </div>
 					<footer>
-						<a href="">
+						<div style="display: inline-block;" @click="changeShow1">
 							<span>更多</span>
 							<i class="icon-more"></i>
-						</a>
+						</div>
 					</footer>
 				</div>
 			</Col>
 			<Col span="12">
 				<div class="ivu-hse-table">
-					<h4>我的项目</h4>
-					<Table stripe :columns="table2.th" :data="table2.td" :row-class-name="rowClassName"></Table>
+					<h4 style="font-size: 18px;">我的项目</h4>
+					<!-- <Table stripe :columns="table2.th" :data="table2.td" :row-class-name="rowClassName"></Table> -->
+					<div class="tableWrap" v-bind:class="{ showHide: showHide2 }" style="margin-top: 10px;">
+                            <div class="tableRow backgroundGrey" style="font-weight: bold;height: 40px;">
+                                <span class="tableSpan1" style="height: 40px;line-height: 40px;">项目编码</span>
+                                <span class="tableSpan1" style="height: 40px;line-height: 40px;">项目名称</span>
+                                <span class="tableSpan1" style="height: 40px;line-height: 40px;">项目类别</span>
+                                <span class="tableSpan1" style="height: 40px;line-height: 40px;">阶段</span>
+                                <span class="tableSpan1" style="height: 40px;line-height: 40px;">状态</span>
+                            </div>
+                            <div v-for="(item, index) in data7">
+                                <div class="tableRow" :class="{ backgroundGrey: item.backgroundGrey,backgroundGreen:item.backgroundGreen,backgroundPink:item.backgroundPink }">
+                                    <span class="tableSpan1">{{item.projectcode}}</span>
+                                    <span class="tableSpan1">{{item.projectname}}</span>
+                                    <span class="tableSpan1">{{item.projecttype}}</span>
+                                    <span class="tableSpan1">{{item.projectstage}}</span>
+                                    <span class="tableSpan1">{{item.projectstatus}}</span>
+                                </div>
+                            </div>
+                    </div>
 					<footer>
-						<a href="">
+						<div style="display: inline-block;" @click="changeShow2">
 							<span>更多</span>
 							<i class="icon-more"></i>
-						</a>
+						</div>
 					</footer>
 				</div>
 			</Col>
@@ -39,14 +82,14 @@
 			    </Select>
 			</header>
 			<Row>
-				<Col span="3" class="steps step1">
+				<Col span="3" class="steps" style="width: 150px;">
 					<span>立项数: <em>360</em></span>
 				</Col>
-				<Col span="1" style="width: 20px;">
+				<Col span="2" style="width: 20px;">
 					<img src="../../assets/arrowRight.png" style="width: 36px;">
 				</Col>
 				<!-- <Col span="3" class="steps step2" style="background:url('../../assets/lingxing.png') no-repeat 0 0;"> -->
-				<Col span="3" class="steps step2">
+				<Col span="3" class="steps">
 					<span>
 						报审数: <em>350</em>
 					</span>
@@ -62,14 +105,14 @@
 					<span class="status-success">实验完成数: <em>120</em></span>
 					<span class="status-error">实验失败数: <em>120</em></span>
 				</Col>
-				<Col span="8" class="nums">
-					<dl>
-						<dt><h3>年度人物分析</h3></dt>
+				<Col span="6" class="nums">
+					<dl style="background-color: #fff;">
+						<dt><h3>年度任务分析</h3></dt>
 						<dd>目标额: </dd>
 						<dd>完成额: </dd>
 						<dd>完成排名: </dd>
 					</dl>
-					<dl>
+					<dl style="background-color: #fff;margin-left: 40px;">
 						<dt><h3>月度任务分析</h3></dt>
 						<dd>目标额: </dd>
 						<dd>完成额: </dd>
@@ -80,7 +123,7 @@
 		</div>
 
 		<!-- table -->
-		<Row class="ivu-hse-noborder">
+		<Row class="ivu-hse-noborder" style="padding-left: 15px;padding-top: 15px;padding-right: 15px;padding-bottom: 15px;background-color: #fff;">
 			<Col span="24">
 				<Table :columns="table3.th" :data="table3.td"></Table>
 			</Col>
@@ -96,7 +139,150 @@
 
 		data () {
 			return {
+				data6:[{
+					plantformname:"W3D电装盒固定板结构优化",
+					plantformtime:"2017-06-21",
+					plantformtype:"审核",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					plantformname:"W3D电装盒固定板结构优化",
+					plantformtime:"2017-06-21",
+					plantformtype:"审核",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					plantformname:"W3D电装盒固定板结构优化",
+					plantformtime:"2017-06-21",
+					plantformtype:"审核",
+					backgroundGreen:false,
+					backgroundPink:false,
+					backgroundGrey:true
+				},{
+					plantformname:"W3D电装盒固定板结构优化",
+					plantformtime:"2017-06-21",
+					plantformtype:"审核",
+					backgroundGreen:false,
+					backgroundPink:true,
+					backgroundGrey:false
+				},{
+					plantformname:"W3D电装盒固定板结构优化",
+					plantformtime:"2017-06-21",
+					plantformtype:"审核",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					plantformname:"W3D电装盒固定板结构优化",
+					plantformtime:"2017-06-21",
+					plantformtype:"审核",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					plantformname:"W3D电装盒固定板结构优化",
+					plantformtime:"2017-06-21",
+					plantformtype:"审核",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				}],
+				data7:[{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:false,
+					backgroundPink:false,
+					backgroundGrey:true
+				},{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:false,
+					backgroundPink:true,
+					backgroundGrey:false
+				},{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				},{
+					projectcode:"BM362312",
+					projectname:"W3D电装盒固定板结构",
+					projecttype:"设计类",
+					projectstage:"试验中",
+					projectstatus:"正常",
+					backgroundGreen:true,
+					backgroundPink:false,
+					backgroundGrey:false
+				}],
 				model1:"",
+				showHide1:false,
+				showHide2:false,
 				table1: {
 					th: [
 						{ title: '项目名称' , key: 'name' } ,
@@ -157,6 +343,12 @@
                     return 'status-error';
                 }
                 return '';
+            },
+            changeShow1:function(){
+            	this.showHide1 = !this.showHide1;
+            },
+            changeShow2:function(){
+            	this.showHide2 = !this.showHide2;
             }
         }
 
@@ -164,7 +356,15 @@
 
 </script>
 
-<style>
+<style scoped>
+.header{
+    height: 40px;
+    background-color: #fff;
+    text-align: right;
+    margin-bottom: 20px;
+    line-height: 40px;
+    padding-right: 20px;
+}
 .ivu-hse-table {
   background: #fff;
   padding: 0 15px;
@@ -358,5 +558,41 @@ i.icon-more:after {
 .status-error {
   background: #f2b8b7 !important;
 }
-
+.tableWrap{
+    border-top: 3px solid #01C0C8;
+    border-left: 1px solid #E9EAEC;
+    border-right: 1px solid #E9EAEC;
+    border-bottom: 1px solid #E9EAEC;
+    height: 300px;
+    overflow: hidden;
+}
+.showHide{
+	height: 100%;
+}
+.tableRow{
+    height: 48px;
+    border-bottom: 1px solid #E9EAEC;
+    text-align: center;
+}
+.tableSpan{
+    width: 30%;
+    display: inline-block;
+    font-size: 12px;
+    line-height: 48px;
+}
+.tableSpan1{
+    width: 19%;
+    display: inline-block;
+    font-size: 12px;
+    line-height: 48px;
+}
+.backgroundGrey{
+    background-color: #F8F8F9;
+}
+.backgroundGreen{
+    background-color: #CEEBBF;
+}
+.backgroundPink{
+    background-color: #F8DAB2;
+}
 </style>
